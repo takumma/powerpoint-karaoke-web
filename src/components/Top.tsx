c6e98a8624940e77b91bfd4d069015bc2f6f2256
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-12-02 22:10:32 +0900

    Add: select

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 function Top() {
+  const slideNum = 3;
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
+        <FormControl>
+          <InputLabel id="slideNum-select-label">slidenum</InputLabel>
+          <Select
+            labelId="slideNum-select-label"
+            value={slideNum}
+          >
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={4}>4</MenuItem>
+            <MenuItem value={5}>5</MenuItem>
+          </Select>
+        </FormControl>
         <Link to='/slide'>Start</Link>
-      </header>
+      </div>
     </div>
   );
 }
