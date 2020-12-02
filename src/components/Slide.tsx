71a729b7d7b0a3ee1465024cead7ac885385ef9d
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-12-02 16:54:27 +0900

    Add: router

---
+++ src/components/Slide.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function Slide() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default Slide;
---
+++ src/components/Top.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function Top() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+function getSlidePictures(num: number) {
+  console.log(num.toString());
+}
+
+export default Top;
